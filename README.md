226f581b118cb22de573cbd0eefd60f113d8be6a
Ashrith K S <ashks@cisco.com>
2019-04-16 13:57:27 +0530

    store changes

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 # react-native-secure-store
+  React Native Library for securely storing keys to iOS and Android devices in KeyChain and KeyStore respectively
 
 ## Getting started
 
@@ -37,9 +38,53 @@
 
 ## Usage
 ```javascript
-import RNSecureStore from 'react-native-secure-store';
+import RNSecureStore, {ACCESSIBLE} from "react-native-secure-store";
 
-// TODO: What to do with the module?
-RNSecureStore;
+// For storing key
+RNSecureStore.set("key1", "value1", {accessible: ACCESSIBLE.ALWAYS_THIS_DEVICE_ONLY})
+	.then((res) => {
+		console.log(res);
+	}, (err) => {
+		console.log(err);
+	});
+
+// For retrieving key
+RNSecureStore.get("key1")
+	.then((res) => {
+		console.log(res);
+	}, (err) => {
+		console.log(err);
+	});
+
+// For removing key
+RNSecureStore.remove("key1")
+	.then((res) => {
+		console.log(res);
+	}, (err) => {
+		console.log(err);
+	});	
 ```
   
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2019 ashrithks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- android/src/main/java/com/securestore/RNSecureStoreModule.java
+++ android/src/main/java/com/securestore/RNSecureStoreModule.java
@@ -6,6 +6,35 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import android.content.Context;
+import android.os.Build;
+import android.security.KeyPairGeneratorSpec;
+import android.util.Log;
+
+import com.facebook.react.bridge.Promise;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.GeneralSecurityException;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Calendar;
+import android.support.annotation.Nullable;
+import com.facebook.react.bridge.ReadableMap;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.security.auth.x500.X500Principal;
+
 public class RNSecureStoreModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
@@ -18,5 +47,176 @@ public class RNSecureStoreModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNSecureStore";
+  }
+
+  @ReactMethod
+  public void set(String alias, String input, @Nullable ReadableMap options, Promise promise) {
+    try {
+      setCipherText(alias, input);
+      promise.resolve("stored ciphertext in app storage");
+    } catch (Exception e) {
+      e.printStackTrace();
+      Log.e(Constants.TAG, "Exception: " + e.getMessage());
+      promise.reject("{\"code\":9,\"api-level\":" + Build.VERSION.SDK_INT + ",\"message\":" + e.getMessage() + "}");
+    }
+  }
+
+  private PublicKey getOrCreatePublicKey(String alias) throws GeneralSecurityException, IOException {
+    KeyStore keyStore = KeyStore.getInstance(getKeyStore());
+    keyStore.load(null);
+
+    if (!keyStore.containsAlias(alias) || keyStore.getCertificate(alias) == null) {
+      Log.i(Constants.TAG, "no existing asymmetric keys for alias");
+
+      Calendar start = Calendar.getInstance();
+      Calendar end = Calendar.getInstance();
+      end.add(Calendar.YEAR, 50);
+      KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(getContext())
+          .setAlias(alias)
+          .setSubject(new X500Principal("CN=" + alias))
+          .setSerialNumber(BigInteger.ONE)
+          .setStartDate(start.getTime())
+          .setEndDate(end.getTime())
+          .build();
+
+      KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", getKeyStore());
+      generator.initialize(spec);
+      generator.generateKeyPair();
+
+      Log.i(Constants.TAG, "created new asymmetric keys for alias");
+    }
+
+    return keyStore.getCertificate(alias).getPublicKey();
+  }
+
+  private byte[] encryptRsaPlainText(PublicKey publicKey, byte[] plainTextBytes) throws GeneralSecurityException, IOException {
+    Cipher cipher = Cipher.getInstance(Constants.RSA_ALGORITHM);
+    cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+    return encryptCipherText(cipher, plainTextBytes);
+  }
+
+  private byte[] encryptAesPlainText(SecretKey secretKey, String plainText) throws GeneralSecurityException, IOException {
+    Cipher cipher = Cipher.getInstance(Constants.AES_ALGORITHM);
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+    return encryptCipherText(cipher, plainText);
+  }
+
+  private byte[] encryptCipherText(Cipher cipher, String plainText) throws GeneralSecurityException, IOException {
+    return encryptCipherText(cipher, plainText.getBytes("UTF-8"));
+  }
+
+  private byte[] encryptCipherText(Cipher cipher, byte[] plainTextBytes) throws GeneralSecurityException, IOException {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    CipherOutputStream cipherOutputStream = new CipherOutputStream(outputStream, cipher);
+    cipherOutputStream.write(plainTextBytes);
+    cipherOutputStream.close();
+    return outputStream.toByteArray();
+  }
+
+  private SecretKey getOrCreateSecretKey(String alias) throws GeneralSecurityException, IOException {
+    try {
+      return getSymmetricKey(alias);
+    } catch (FileNotFoundException fnfe) {
+      Log.i(Constants.TAG, "no existing symmetric key for alias");
+
+      KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+      //32bytes / 256bits AES key
+      keyGenerator.init(256);
+      SecretKey secretKey = keyGenerator.generateKey();
+      PublicKey publicKey = getOrCreatePublicKey(alias);
+      Storage.writeValues(getContext(), Constants.SKS_KEY_FILENAME + alias,
+          encryptRsaPlainText(publicKey, secretKey.getEncoded()));
+
+      Log.i(Constants.TAG, "created new symmetric keys for alias");
+      return secretKey;
+    }
+  }
+
+  private void setCipherText(String alias, String input) throws GeneralSecurityException, IOException {
+    Storage.writeValues(getContext(), Constants.SKS_DATA_FILENAME + alias,
+        encryptAesPlainText(getOrCreateSecretKey(alias), input));
+  }
+
+  @ReactMethod
+  public void get(String alias, Promise promise) {
+    try {
+      promise.resolve(getPlainText(alias));
+    } catch (FileNotFoundException fnfe) {
+      fnfe.printStackTrace();
+      promise.reject("404", "{\"code\":404,\"api-level\":" + Build.VERSION.SDK_INT + ",\"message\":" + fnfe.getMessage() + "}", fnfe);
+    } catch (Exception e) {
+      e.printStackTrace();
+      Log.e(Constants.TAG, "Exception: " + e.getMessage());
+      promise.reject("{\"code\":1,\"api-level\":" + Build.VERSION.SDK_INT + ",\"message\":" + e.getMessage() + "}");
+    }
+  }
+
+  private PrivateKey getPrivateKey(String alias) throws GeneralSecurityException, IOException {
+    KeyStore keyStore = KeyStore.getInstance(getKeyStore());
+    keyStore.load(null);
+    return (PrivateKey) keyStore.getKey(alias, null);
+  }
+
+  private byte[] decryptRsaCipherText(PrivateKey privateKey, byte[] cipherTextBytes) throws GeneralSecurityException, IOException {
+    Cipher cipher = Cipher.getInstance(Constants.RSA_ALGORITHM);
+    cipher.init(Cipher.DECRYPT_MODE, privateKey);
+    return decryptCipherText(cipher, cipherTextBytes);
+  }
+
+  private byte[] decryptAesCipherText(SecretKey secretKey, byte[] cipherTextBytes) throws GeneralSecurityException, IOException {
+    Cipher cipher = Cipher.getInstance(Constants.AES_ALGORITHM);
+    cipher.init(Cipher.DECRYPT_MODE, secretKey);
+    return decryptCipherText(cipher, cipherTextBytes);
+  }
+
+  private byte[] decryptCipherText(Cipher cipher, byte[] cipherTextBytes) throws IOException {
+    ByteArrayInputStream bais = new ByteArrayInputStream(cipherTextBytes);
+    CipherInputStream cipherInputStream = new CipherInputStream(bais, cipher);
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    byte[] buffer = new byte[256];
+    int bytesRead = cipherInputStream.read(buffer);
+    while (bytesRead != -1) {
+      baos.write(buffer, 0, bytesRead);
+      bytesRead = cipherInputStream.read(buffer);
+    }
+    return baos.toByteArray();
+  }
+
+  private SecretKey getSymmetricKey(String alias) throws GeneralSecurityException, IOException {
+    byte[] cipherTextBytes = Storage.readValues(getContext(), Constants.SKS_KEY_FILENAME + alias);
+    return new SecretKeySpec(decryptRsaCipherText(getPrivateKey(alias), cipherTextBytes), Constants.AES_ALGORITHM);
+  }
+
+  private String getPlainText(String alias) throws GeneralSecurityException, IOException {
+    SecretKey secretKey = getSymmetricKey(alias);
+    byte[] cipherTextBytes = Storage.readValues(getContext(), Constants.SKS_DATA_FILENAME + alias);
+    return new String(decryptAesCipherText(secretKey, cipherTextBytes), "UTF-8");
+  }
+
+  @ReactMethod
+  public void remove(String alias, Promise promise) {
+    Storage.resetValues(getContext(), new String[] { 
+      Constants.SKS_DATA_FILENAME + alias, 
+      Constants.SKS_KEY_FILENAME + alias, 
+    });
+    promise.resolve("cleared alias");
+  }
+
+  private Context getContext() {
+    return getReactApplicationContext();
+  }
+
+  private String getKeyStore() {
+    try {
+      KeyStore.getInstance(Constants.KEYSTORE_PROVIDER_1);
+      return Constants.KEYSTORE_PROVIDER_1;
+    } catch (Exception err) {
+      try {
+        KeyStore.getInstance(Constants.KEYSTORE_PROVIDER_2);
+        return Constants.KEYSTORE_PROVIDER_2;
+      } catch (Exception e) {
+        return Constants.KEYSTORE_PROVIDER_3;
+      }
+    }
   }
 }
